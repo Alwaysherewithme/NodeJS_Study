d5d076dc6f34da5fbdff11a35ffd5244fce7bedb
Wenming W Zhou <wzhou047@nam.ad.pwcinternal.com>
2018-05-01 22:10:46 +0800

    NodeJS internal/external funciton demo...

---
+++ modules/module_1.js
@@ -0,0 +1,23 @@
+﻿// 外部函数
+function fun2(res) {
+    console.log("External JS function: fun2(){...}");
+    res.write("This is fun2(){...}<br/>");
+}
+
+//module.exports = fun2;  // 只支持一个函数
+
+// 支持多个函数
+module.exports = {
+    fun2: function(res) {
+        console.log("This is fun2 in external JS function...");
+        res.write("Hi, I'm fun2 in external JS function.<br/>");
+    },
+    fun3: function(res) {
+        console.log("This is fun3 in external JS function...");
+        res.write("Hi, I'm fun3 in external JS function.<br/>");
+    },
+    fun4: function(res) {
+        console.log("This is fun4 in external JS function...");
+        res.write("Hi, I'm fun4 in external JS function.<br/>");
+    }
+}
---
+++ nodejs_2.js
@@ -0,0 +1,26 @@
+﻿var http = require("http");
+var otherfun = require("./modules/module_1.js");
+http.createServer(function (request, response) {
+    response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+
+    if(request.url!="/favicon.ico"){
+        console.log("Visiting……");
+        fun1(response);  // 调用本地函数
+        //otherfun(response);  // 调用只有一个函数的外部函数，这里使用上文定义的变量otherfun
+        // 用字符串调用对应的函数
+        funname = "fun4";
+        otherfun[funname](response);
+        otherfun.fun2(response);
+        otherfun['fun3'](response);
+        response.end("(^v^)");
+    }
+    
+}).listen(8888);
+
+console.log("*****Server running at http://127.0.0.1:8888/*****");
+
+// 本地函数
+function fun1(res) {
+    console.log("Local JS function: fun1(){...}");
+    res.write("This is fun1(){...}<br/>");
+}
