cd9c3e4c2db04632f7772026642149fa780d589a
Wenming W Zhou <wzhou047@nam.ad.pwcinternal.com>
2018-05-02 10:18:38 +0800

    Updated NodeJS module demo...

--- modules/module_2_teacher.js
+++ modules/module_2_teacher.js
@@ -1,8 +1,9 @@
 ﻿var UserEmployee = require("./module_2_user");
 
 function Teacher(eid, ename, eage) {
-    UserEmployee.employee.apply(this, [eid, ename, eage]);  // Teacher继承UserEmployee
-    this.teach = function(res) {
+    UserEmployee.employee.apply(this, [eid, ename, eage]);  // Teacher继承UserEmployee.employee
+
+    this.teach = function(res) {  // Teacher自身的方法teach()
         res.write(this.ename + " is teaching...");
     }
 }
