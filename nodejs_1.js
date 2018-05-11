508819d245d40ad803860b1903808c3a132cbfeb
Wenming W Zhou <wzhou047@nam.ad.pwcinternal.com>
2018-05-11 10:15:52 +0800

    NodeJS HTTP,URL related modules...

--- nodejs_1.js
+++ nodejs_1.js
@@ -1,4 +1,6 @@
-﻿var http = require("http");
+﻿//The Built-in HTTP Module
+
+var http = require("http");
 http.createServer(function (request, response) {
     // 发送HTTP头部，HTTP状态值：200: OK，内容类型：text/plain
     response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
@@ -13,3 +15,18 @@ http.createServer(function (request, response) {
 }).listen(8888);
 // 终端打印如下信息
 console.log("*****Server running at http://127.0.0.1:8888/*****");
+
+
+
+// The Built-in URL Module
+var url = require('url');
+
+var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+var q = url.parse(adr, true);
+
+console.log(q.host); // 'localhost:8080'
+console.log(q.pathname); // '/default.htm'
+console.log(q.search); // '?year=2017&month=february'
+
+var qdata = q.query; // object: { year: 2017, month: 'february' }
+console.log(qdata.month); // 'february'
